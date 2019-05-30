52f22006519833f0efc49d8a38e694392d2692b3
AntonStriga <glau_76@mail.ru>
2019-05-30 13:50:15 +0300

    Added methods: waitAndClick & waitAndSendKeys

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,9 +3,14 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+
 
 public class FirstTest {
 
@@ -18,10 +23,10 @@ public class FirstTest {
 
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","6.0");
+        capabilities.setCapability("platformVersion","8.0");
         capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity","org.wikipedia.main.MainActivity");
+        capabilities.setCapability("appActivity",".main.MainActivity");
         capabilities.setCapability("app","d:/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
@@ -34,8 +39,61 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest()
+    public void firstTest() throws InterruptedException
     {
-        System.out.println("Firts test run");
+        // Preconditions
+        WebElement element = driver.findElement(By.id("org.wikipedia:id/fragment_onboarding_skip_button"));
+        element.click();
+        // -----
+
+        waitForElementByXpathAndClick(
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Cannot find skip button",
+                5
+        );
+
+        waitForElementByXpathAndSenKeys(
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementPresentByXpath(
+                "//*[@class='android.view.ViewGroup']//*[@text='Object-oriented programming language']",
+                "Cannot find content",
+                15
+        );
+
+        System.out.println("First test run");
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message)
+    {
+        return waitForElementPresentByXpath(xpath, error_message, 5);
+    }
+
+    private WebElement waitForElementByXpathAndClick(String xpath, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresentByXpath(xpath, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementByXpathAndSenKeys(String xpath, String value, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresentByXpath(xpath, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
     }
 }
