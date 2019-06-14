579241319e863e4e55392177eaa3f6fe1ec42aa9
AntonStriga <glau_76@mail.ru>
2019-06-14 15:57:57 +0300

    Ready FrameWork

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -3,6 +3,7 @@ package lib;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -21,11 +22,12 @@ public class CoreTestCase extends TestCase{
 
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","6.0");
+        capabilities.setCapability("platformVersion","8.0");
         capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
         capabilities.setCapability("app","d:/JavaAppiumAutomation/apks/old.org.wikipedia.apk");
+        capabilities.setCapability("orientation","PORTRAIT");
 
         driver = new AndroidDriver(new URL(AppiumUrl), capabilities);
     }
@@ -36,5 +38,20 @@ public class CoreTestCase extends TestCase{
         driver.quit();
 
         super.tearDown();
+    }
+
+    protected void rotateScreenPortrait()
+    {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+    }
+
+    protected void rotateScreenLandscape()
+    {
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+    }
+
+    protected void backgroundApp(int seconds)
+    {
+        driver.runAppInBackground(seconds);
     }
 }
